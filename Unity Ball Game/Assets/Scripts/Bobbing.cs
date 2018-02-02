ef16624cb31628b36f2bf918599c4b9d15c13d7c
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2018-02-02 13:18:43 -0500

    i hate this

---
+++ Unity Ball Game/Assets/Scripts/Bobbing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bobbing : MonoBehaviour {
+	
+	private int direction = 1;
+	public float distanceRisen;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(new Vector3(15, 30, 25) * Time.deltaTime);
+
+		transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y + Random.Range(0.001f, 0.1f)*direction, transform.localPosition.z);
+		if(transform.localPosition.y > distanceRisen)
+		{
+			direction = -1;
+		}
+		else if(transform.localPosition.y <= 0.5f)
+		{
+			direction = 1;
+		}
+	}
+}
---
+++ Unity Ball Game/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Controller : MonoBehaviour {
+	
+	public float speed;
+	private Rigidbody sphereRB;
+	public float jumpForce;
+	private bool isGrounded;
+	private int score;
+	public Text scoreText;
+	public Text winText;
+	private int gameState = 0;
+	private Vector3 startPos;
+	
+	void Start()
+	{
+		sphereRB = GetComponent<Rigidbody>();
+		score = 0;
+		setScoreText();
+		winText.text = "";
+		startPos = transform.position;
+	}
+	
+	void FixedUpdate()
+    {
+		if(gameState == 0)
+		{
+			//Horizontal movement
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
+			
+			Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+			
+			sphereRB.AddForce(movement * speed);
+			
+			//Jump 
+			isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.5f);
+			
+			if(isGrounded && Input.GetKey(KeyCode.Space))
+			{
+				sphereRB.AddForce(0, jumpForce, 0);
+			}
+			
+			if(transform.position.y < -5)
+			{
+				transform.position = startPos;
+			}
+		}
+		else if(gameState == 1)
+		{
+			winText.text = "You win!";
+		}
+    }
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		//Destroy(other.gameObject);
+		if(gameState == 0)
+		{
+			if(other.gameObject.CompareTag("Pick Up"))
+			{
+				other.gameObject.SetActive(false);
+				score++;
+				setScoreText();
+			}
+		}
+	}
+	
+	void setScoreText()
+	{
+		scoreText.text = "Score: " + score.ToString();
+		if(score >= 12)
+		{
+			gameState = 1;
+		}
+	}
+}
